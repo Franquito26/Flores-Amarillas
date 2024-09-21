e1dd86edf2c3f643b9ef746d40f761017dbdc88a
Franco <fran260602@gmail.com>
2024-09-20 23:32:03 -0300

    Se agrego musica y fixeo flores

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-onload = () =>{
+window.onload = () => {
         document.body.classList.remove("container");
-};
+    
+        const audio = document.getElementById('myAudio');
+    
+        // Intenta reproducir el audio
+        audio.play().then(() => {
+            localStorage.setItem('audioPlaying', 'true');
+        }).catch((error) => {
+            console.log("Error al reproducir audio:", error);
+        });
+    };
+    
